338603ea6ada4f5a9e4b66df1bf701fc6fbbb84e
Dean Fogarty <dfogarty@raywhite.com>
2023-02-21 13:33:30 +1000

    Add Leaflet.

---
+++ src/lib/Leaflet.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import L from 'leaflet';
+
+    let map;
+
+    onMount(() => {
+        map = L.map('map', {
+            center: [ 51.505, -0.09 ],
+            zoom: 13,
+        });
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    });
+
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
+</svelte:head>
+
+<div id="map"></div>
+
+<style>
+    #map {
+        height: 180px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import MapLeaflet from '$lib/Leaflet.svelte';
+    import MapOpenLayers from '$lib/OpenLayers.svelte';
+    import MapMapLibre from '$lib/MapLibre.svelte';
+</script>
+
+<MapLeaflet />
+<MapOpenLayers />
+<MapMapLibre />
